34368ca833cb3008fd3c75861f73f384b2371c6d
Mac Sweeny <macsweenydev@gmail.com>
2023-01-12 11:46:08 +1000

    Discord order

--- apps/dashboard/src/server/trpc/router/discord.ts
+++ apps/dashboard/src/server/trpc/router/discord.ts
@@ -3,6 +3,30 @@ import { router, protectedProcedure } from "../trpc";
 import type { DiscordGuild } from "../../../types";
 
 export const discordRouter = router({
+
+  createDiscordGuild: protectedProcedure
+  .input(
+    z.object({
+      name: z.string(),
+      discordId: z.string(),
+      communityId: z.string(),
+    })
+  )
+  .mutation(async ({ input, ctx }) => {
+    const guild = await ctx.prisma.discordGuild.create({
+      data: {
+        name: input.name,
+        discordId: input.discordId,
+        community: {
+          connect: {
+            id: input.communityId,
+          },
+        },
+      },
+    });
+    return guild;
+  }),
+
   getDiscordOwnedGuilds: protectedProcedure
     .input(
       z.object({
@@ -32,27 +56,5 @@ export const discordRouter = router({
         }
         console.log("No Guilds Found");
       }
-    }),
-  createDiscordGuild: protectedProcedure
-    .input(
-      z.object({
-        name: z.string(),
-        discordId: z.string(),
-        communityId: z.string(),
-      })
-    )
-    .mutation(async ({ input, ctx }) => {
-      const guild = await ctx.prisma.discordGuild.create({
-        data: {
-          name: input.name,
-          discordId: input.discordId,
-          community: {
-            connect: {
-              id: input.communityId,
-            },
-          },
-        },
-      });
-      return guild;
     }),
 });
