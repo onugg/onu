12664092a01c3d66d27f3af9c2814ea4d1bd23c3
Tim O'Connell <tim.oconnell.australia@gmail.com>
2023-01-17 17:53:16 +1100

    remove unnecessary console log

--- packages/prisma/extensions/quests/memberQuestRecord.ts
+++ packages/prisma/extensions/quests/memberQuestRecord.ts
@@ -41,8 +41,6 @@ export async function updateAndEmitLevelUpEvent(emitEventCallBack: Function, mem
         discordUserId: dbMember.user.discordUser.discordId,
         level: newLevel
       }
-
-      console.log(questMemberLevelUpMessage)
   
       emitEventCallBack(OnuKafkaTypes.Prisma.QuestMemberLevelUp, questMemberLevelUpMessage)
     }
