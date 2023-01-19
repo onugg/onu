abe247f73a2a739a84e60c9ef3a867cdba1d4557
Tim O'Connell <tim.oconnell.australia@gmail.com>
2023-01-20 08:00:31 +1100

    add scheduler and schedule reset weekly streaks

--- packages/kafka/interfaces.ts
+++ packages/kafka/interfaces.ts
@@ -2,15 +2,17 @@ export * as DiscordBot from '@onu/discord-bot/src/bot/kafkaProducers'
 export * as Prisma from '@onu/prisma/kafkaProducers'
 export * as MetricTracker from '@onu/metric-tracker/src/kafkaProducers'
 export * as QuestTracker from '@onu/quest-tracker/src/kafkaProducers'
+export * as Scheduler from '@onu/scheduler/src/kafkaProducers'
 
 import { DiscordBotTopics } from '@onu/discord-bot/src/bot/kafkaProducers'
 import { PrismaTopics } from '@onu/prisma/kafkaProducers'
 import { MetricTrackerTopics } from '@onu/metric-tracker/src/kafkaProducers'
 import { QuestTrackerTopics } from '@onu/quest-tracker/src/kafkaProducers'
+import { SchedulerTopics } from '@onu/scheduler/src/kafkaProducers'
 
-export { DiscordBotTopics, PrismaTopics, MetricTrackerTopics, QuestTrackerTopics }
+export { DiscordBotTopics, PrismaTopics, MetricTrackerTopics, QuestTrackerTopics, SchedulerTopics }
 
-export const AllTopics = DiscordBotTopics.concat(PrismaTopics, MetricTrackerTopics, QuestTrackerTopics)
+export const AllTopics = DiscordBotTopics.concat(PrismaTopics, MetricTrackerTopics, QuestTrackerTopics, SchedulerTopics)
 
 export interface KafkaOptions {
   clientId: string;
