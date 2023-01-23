e8c45e6b7372319dc1e49925339ba4bd7894d7d0
Mac Sweeny <macsweenydev@gmail.com>
2023-01-23 23:19:50 +1000

    Cards + misc

--- apps/dashboard/src/components/ui/community/communityCard.tsx
+++ apps/dashboard/src/components/ui/community/communityCard.tsx
@@ -45,38 +45,25 @@ const CommunityCard: React.FC<CommunityCardProps> = ({
 }) => {
   return (
     <div>
-      <Link
-        href={`/c/${slug}`}
-        className="group relative flex flex-col duration-300 hover:scale-105"
-      >
+      <div className="relative flex flex-col">
         <div
-          className=" relative flex h-40 justify-center rounded-t-xl border border-neutral-700 bg-black bg-cover text-sm duration-300 group-hover:border-2 group-hover:border-neutral-200 group-hover:text-neutral-100 "
+          className="relative flex h-40 rounded-t-xl border-neutral-700 bg-black bg-cover text-sm duration-300 group-hover:border-neutral-200 group-hover:text-neutral-100 "
           style={{
             backgroundImage: `url(${image})`,
           }}
-        >
-          <div className="z-5 absolute inset-0 rounded-t-xl bg-black/75 backdrop-blur-md" />
-          <div className="z-10 flex items-center overflow-hidden">
-            <Image
-              src={image}
-              alt="Community"
-              className="h-[100px] w-[100px] rounded-full border border-neutral-700 duration-300 group-hover:border-neutral-100"
-              width="128"
-              height="128"
-            />
+        />
+        <div className="relative flex h-40 flex-col justify-between rounded-b-xl border-neutral-700 border-t-transparent bg-theme-800 px-4 py-4 duration-300 group-hover:border-neutral-200 group-hover:border-t-transparent">
+          <div className="text-base font-semibold text-neutral-200">{name}</div>
+          <div className="text-sm font-normal text-neutral-400 line-clamp-2">
+            {description}
           </div>
-        </div>
-        <div className="relative flex h-40 rounded-b-xl border border-neutral-700 border-t-transparent bg-black duration-300 group-hover:border-2 group-hover:border-neutral-200 group-hover:border-t-transparent group-hover:text-neutral-100">
-          <div className="mx-4 my-4 flex flex-col justify-between gap-2">
-            <div className="flex flex-col gap-2">
-              <div className="text-base font-semibold text-neutral-200">
-                {name}
-              </div>
-              <div className="text-sm font-normal text-neutral-500 line-clamp-3">
-                {description}
-              </div>
-            </div>
-            <div className="flex justify-start gap-8 text-[13px] font-normal text-neutral-500">
+          <Link
+            className="flex h-10 w-full justify-center items-center text-sm font-bold rounded-lg bg-violet-500 text-white hover:bg-violet-700 hover:scale-105 duration-300"
+            href={`/c/${slug}`}
+          >
+            {`Go to ${name}`}
+          </Link>
+          {/* <div className="flex justify-start gap-8 text-[13px] font-normal text-neutral-500">
               <div className="flex flex-row items-center justify-center gap-1">
                 <GreenDot />
                 {activeMembers} Online
@@ -85,10 +72,9 @@ const CommunityCard: React.FC<CommunityCardProps> = ({
                 <GreyDot />
                 {totalMembers} Members
               </div>
-            </div>
-          </div>
+            </div> */}
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
