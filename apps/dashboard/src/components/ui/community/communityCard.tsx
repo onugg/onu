d3a90fb7b71e70d68fd6f3b76c839ad650635b54
Mac Sweeny <macsweenydev@gmail.com>
2023-02-03 20:13:43 +1000

    fix: communityCard styling

--- apps/dashboard/src/components/ui/community/communityCard.tsx
+++ apps/dashboard/src/components/ui/community/communityCard.tsx
@@ -47,18 +47,18 @@ const CommunityCard: React.FC<CommunityCardProps> = ({
     <div>
       <div className="relative flex flex-col">
         <div
-          className="relative flex h-40 rounded-t-xl border-neutral-700 bg-black bg-cover text-sm duration-300 group-hover:border-neutral-200 group-hover:text-neutral-100 "
+          className="relative flex h-40 rounded-t-xl bg-black bg-cover bg-center text-sm duration-300 group-hover:text-neutral-100 "
           style={{
             backgroundImage: `url(${image})`,
           }}
         />
-        <div className="relative flex h-40 flex-col justify-between rounded-b-xl border-neutral-700 border-t-transparent bg-theme-800 px-4 py-4 duration-300 group-hover:border-neutral-200 group-hover:border-t-transparent">
-          <div className="text-base font-semibold text-neutral-200">{name}</div>
-          <div className="text-sm font-normal text-neutral-400 line-clamp-2">
+        <div className="relative flex h-40 flex-col justify-between rounded-b-xl bg-gray-900 px-4 py-4 duration-300">
+          <div className="text-base font-semibold text-gray-100">{name}</div>
+          <div className="text-sm font-normal text-gray-300 line-clamp-2">
             {description}
           </div>
           <Link
-            className="flex h-10 w-full justify-center items-center text-sm font-bold rounded-lg bg-violet-500 text-white hover:bg-violet-700 hover:scale-105 duration-300"
+            className="flex h-10 w-full items-center justify-center rounded-lg bg-violet-500 text-sm font-bold text-gray-200 duration-300 hover:bg-violet-700"
             href={slug}
           >
             {`Go to ${name}`}
