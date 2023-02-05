d5410f9b86c5fe5090b863b0a2a418fd52443d7e
Mac Sweeny <macsweenydev@gmail.com>
2023-02-06 00:44:00 +1000

    fix: auth dirs

--- monorepo/packages/auth/src/auth-options.ts
+++ monorepo/packages/auth/src/auth-options.ts
@@ -8,15 +8,8 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 export const authOptions: NextAuthOptions = {
