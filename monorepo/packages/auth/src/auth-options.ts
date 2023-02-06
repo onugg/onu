85d76a3cf10bbb477caa22fdd49aa81983b10a02
Mac Sweeny <macsweenydev@gmail.com>
2023-02-06 10:06:25 +1000

    fix: Added new user types

--- monorepo/packages/auth/src/auth-options.ts
+++ monorepo/packages/auth/src/auth-options.ts
@@ -1,9 +1,14 @@
-// export default NextAuth(authOptions);
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type DefaultSession, type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import { prisma } from "@onu/database";
 
+/**
+ * Module augmentation for `next-auth` types
+ * Allows us to add custom properties to the `session` object
+ * and keep type safety
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ **/
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
@@ -12,6 +17,11 @@ declare module "next-auth" {
   }
 }
 
+/**
+ * Options for NextAuth.js used to configure
+ * adapters, providers, callbacks, etc.
+ * @see https://next-auth.js.org/configuration/options
+ **/
 export const authOptions: NextAuthOptions = {
   callbacks: {
     session({ session, user }) {
@@ -28,5 +38,14 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.DISCORD_CLIENT_ID as string,
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
     }),
+    /**
+     * ...add more providers here
+     *
+     * Most other providers require a bit more work than the Discord provider.
+     * For example, the GitHub provider requires you to add the
+     * `refresh_token_expires_in` field to the Account model. Refer to the
+     * NextAuth.js docs for the provider you want to use. Example:
+     * @see https://next-auth.js.org/providers/github
+     **/
   ],
 };
